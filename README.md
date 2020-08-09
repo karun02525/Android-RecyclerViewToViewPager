52c5d388e0f26711cb45aaf2841302907b699c83
rishi02525 <54019705+rishi02525@users.noreply.github.com>
2020-08-09 15:18:48 +0530

    recyclerView or viewpager2

---
+++ README.md
@@ -0,0 +1,8 @@
+#RecyclerViewToViewPager Demo
+
+#RecyclerView to ViewPager2 along with
+
+✔ jetpack components ViewModel.
+✔ LiveData.
+✔ NavigationComponent.
+✔Architecture.
---
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.developersbreach.recyclerviewtoviewpager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.developersbreach.recyclerviewtoviewpager.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
